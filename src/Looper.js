eac32768c07c6b8a9dd54d8c3e717558d8366c99
Doreen <Doreen@DESKTOP-S59AJ7A>
2021-03-12 02:57:56 +0200

    when button is clicked and play is on audio joins in next loop (synced)

--- src/Looper.js
+++ src/Looper.js
@@ -1,18 +1,17 @@
-import React, { Component, useState } from "react";
-import Board from './Board.js';
+import React from "react";
+import Board from "./Board.js";
 
-function Looper () {
-      return (
-        <div className="looper">
-          <div className="looper-board">
-            <Board />
-          </div>
-          <div className="looper-info">
-            <div>{}</div>
-            <ol>{}</ol>
-          </div>
-        </div>
-      );
-    
-  }
-  export default Looper;
+function Looper() {
+  return (
+    <div className="looper">
+      <div className="looper-board">
+        <Board />
+      </div>
+      <div className="looper-info">
+        <div>{}</div>
+        <ol>{}</ol>
+      </div>
+    </div>
+  );
+}
+export default Looper;
